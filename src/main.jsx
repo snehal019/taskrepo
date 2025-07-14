74789c123c6460c5fd8365b21ac2f9a30c40fedc
Snehal patel <snehal.patel019@gmail.com>
2025-07-14 02:20:47 -0700

    downgread vite  or  azure

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import DisplayTask from './DisplayTask.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+     <app></app>
     <DisplayTask></DisplayTask>
   </StrictMode>
 )
